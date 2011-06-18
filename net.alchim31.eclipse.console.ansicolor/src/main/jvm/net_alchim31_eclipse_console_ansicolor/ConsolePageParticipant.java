a80fba0cfb4eb1d99c866b6ef837fe4416264d02
David Bernard <david.bernard.31@gmail.com>
2011-06-18 18:28:27 +0200

    merge AnsiStyleListener into ConsolePageParticipant keep styles set by other Participant (eg of hyperlink, stderr,...)

--- net.alchim31.eclipse.console.ansicolor/src/main/jvm/net_alchim31_eclipse_console_ansicolor/ConsolePageParticipant.java
+++ net.alchim31.eclipse.console.ansicolor/src/main/jvm/net_alchim31_eclipse_console_ansicolor/ConsolePageParticipant.java
@@ -1,12 +1,21 @@
 package net_alchim31_eclipse_console_ansicolor;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.swt.custom.LineStyleEvent;
+import org.eclipse.swt.custom.LineStyleListener;
+import org.eclipse.swt.custom.StyleRange;
 import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.console.IConsole;
 import org.eclipse.ui.console.IConsolePageParticipant;
 import org.eclipse.ui.part.IPageBookViewPage;
 
 public class ConsolePageParticipant implements IConsolePageParticipant {
 
+  private AnsiStyleListener _ansiStyler = new AnsiStyleListener();
+  
   @Override
   public Object getAdapter(Class arg0) {
     return null;
@@ -14,10 +23,12 @@ public class ConsolePageParticipant implements IConsolePageParticipant {
 
   @Override
   public void activated() {
+    System.out.println("activated ConsolePageParticipant");
   }
 
   @Override
   public void deactivated() {
+    System.out.println("deactivated ConsolePageParticipant");
   }
 
   /**
@@ -27,6 +38,7 @@ public class ConsolePageParticipant implements IConsolePageParticipant {
    */
   @Override
   public void dispose() {
+    System.out.println("dispose ConsolePageParticipant");
     Activator.getDefault().removeViewers(this);
   }
 
@@ -42,9 +54,36 @@ public class ConsolePageParticipant implements IConsolePageParticipant {
     if (page.getControl() instanceof StyledText) {
       StyledText viewer = (StyledText) (page.getControl());
       Activator.getDefault().setViewer(viewer, this);
-      viewer.addLineStyleListener(new AnsiStyleListener());
+      viewer.addLineStyleListener(_ansiStyler);
     }
 
+  }
+  
+  static class AnsiStyleListener implements LineStyleListener {
+    
+    // TODO optimize parsing and matching
+    @Override
+    public void lineGetStyle(LineStyleEvent event) {
+      try {
+        if (event.lineText.length() == 0) {
+          return;
+        }
+        List<StyleRange> styles =  AnsiCodesReader.extractStyleRange(event.lineText, event.lineOffset, Display.getCurrent());
+        // override current styles => should be listed after
+        if (event.styles != null) {
+          List<StyleRange> ansiStyles = styles;
+          styles = new ArrayList<StyleRange>(event.styles.length + ansiStyles.size());
+          for (StyleRange sr : event.styles) {
+            styles.add(sr);
+          }
+          styles.addAll(ansiStyles);
+        }
+        event.styles = styles.toArray(new StyleRange[0]);
+      } catch (Exception exc) {
+        //TODO Log
+        exc.printStackTrace();
+      }
+    }
   }
 
 }
